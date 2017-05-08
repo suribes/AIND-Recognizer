7908d61b619329c143e42cff711ef9f65c80a9f5
Sergio Uribe <sergiou@hotmail.com>
2017-05-08 12:53:07 +0200

    Remove log contents when starting and remove formatting from logs

--- utils.py
+++ utils.py
@@ -5,15 +5,15 @@ def config_log():
     logger = logging.getLogger('recognizer')
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
-    fh = logging.FileHandler('recognizer.log')
+    fh = logging.FileHandler('recognizer.log', mode = 'w')
     fh.setLevel(logging.DEBUG)
     # create console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(logging.ERROR)
     # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(levelname)-8s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
+    # formatter = logging.Formatter('%(asctime)s - %(levelname)-8s - %(message)s')
+    # fh.setFormatter(formatter)
+    # ch.setFormatter(formatter)
     # add the handlers to the logger
     logger.addHandler(fh)
     logger.addHandler(ch)
